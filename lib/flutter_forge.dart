5e9eb89559e9dec2e6e9d257e52b86e15519ce25
Drew De Ponte <cyphactor@gmail.com>
2022-11-01 17:57:07 -0400

    Remove ComponentStatefulWidget
    Remove ComponentStatefulWidget because we converted ComponentWidget to
    behind the scenes be a stateful widget and therefore we no longer need
    ComponentStatefulWidget. This also in turn simplifies the use of a
    stateful widget significantly.
    
    ps-id: 2fc5984a-7bbf-4d2f-aa75-09660b941757

--- lib/flutter_forge.dart
+++ lib/flutter_forge.dart
@@ -3,6 +3,5 @@ export 'src/store_interface.dart';
 export 'src/view_store_interface.dart';
 export 'src/reducer_action.dart';
 export 'src/component_widget.dart';
-export 'src/component_stateful_widget.dart';
 export 'src/action_tuple.dart';
 export 'src/effect_task.dart';
