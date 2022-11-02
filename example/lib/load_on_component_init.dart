f191cd7b8c8e415166b552a67d7c2c8bf41204c3
Drew De Ponte <cyphactor@gmail.com>
2022-11-01 22:28:29 -0400

    Rework LoadOnInitComponent to be more realistic
    Rework LoadOnInitComponent to be more realistic. This included adding a
    separate state property, name, that can be updated via the actions and
    effects as we would normally.
    
    This also includes the workaround exploration to deal with initState
    triggered actions throwing an exception.

--- example/lib/load_on_component_init.dart
+++ example/lib/load_on_component_init.dart
@@ -4,8 +4,9 @@ import 'package:flutter_forge/flutter_forge.dart';
 // State definition
 @immutable
 class LoadOnInitComponentState {
-  const LoadOnInitComponentState({required this.count});
+  const LoadOnInitComponentState({required this.count, required this.name});
   final int count;
+  final String name;
 }
 
 class LoadOnInitComponentEnvironment {}
@@ -14,12 +15,27 @@ class LoadOnInitComponentEnvironment {}
 class LoadOnInitCompnoentAction {
   static ActionTuple<LoadOnInitComponentState, LoadOnInitComponentEnvironment>
       load(LoadOnInitComponentState state) {
-    return ActionTuple(const LoadOnInitComponentState(count: 500), null);
+    return ActionTuple(
+        LoadOnInitComponentState(count: state.count, name: "Loading..."),
+        (state, environment) {
+      return Future.delayed(const Duration(seconds: 5), () {})
+          .then((_) => LoadOnInitCompnoentAction.setName("The Loaded Name"));
+    });
+  }
+
+  static ReducerAction<LoadOnInitComponentState, LoadOnInitComponentEnvironment>
+      setName(String name) {
+    return (state) {
+      return ActionTuple(
+          LoadOnInitComponentState(name: name, count: state.count), null);
+    };
   }
 
   static ActionTuple<LoadOnInitComponentState, LoadOnInitComponentEnvironment>
       increment(LoadOnInitComponentState state) {
-    return ActionTuple(LoadOnInitComponentState(count: state.count + 1), null);
+    return ActionTuple(
+        LoadOnInitComponentState(count: state.count + 1, name: state.name),
+        null);
   }
 }
 
@@ -31,26 +47,51 @@ class LoadOnInitComponentWidget extends ComponentWidget<
   factory LoadOnInitComponentWidget.selfContained() {
     return LoadOnInitComponentWidget(
         store: Store(
-            initialState: const LoadOnInitComponentState(count: 0),
+            initialState:
+                const LoadOnInitComponentState(count: 0, name: "Initial"),
             environment: LoadOnInitComponentEnvironment()));
   }
 
   @override
   void initState(viewStore) {
-    viewStore.send(LoadOnInitCompnoentAction.load);
+    // TODO: figure out why this is needed, figure out if there is a better way to handle this
+    // viewStore.send(LoadOnInitCompnoentAction.load);
+    //
+    // The above will cause an exception if it isn't wrapped in a Future.delayed(Duration.zero like below. I found this Future.delayed thing at the following.
+    // https://www.fluttercampus.com/guide/230/setstate-or-markneedsbuild-called-during-build/
+    //
+    // Maybe this is an acceptable solution and we should just bake it into the ComponentWidget initState proxy so that users don't have to think/worry about it
+    //
+    // Note: I have tried doing it without the Future.delayed both before and after the super.initState() with no difference, it still blows up.
+    Future.delayed(
+        Duration.zero, () => viewStore.send(LoadOnInitCompnoentAction.load));
     super.initState(viewStore);
   }
 
   @override
   Widget build(context, state, viewStore) {
-    return Column(children: [
-      Text(
-        '${state.count}',
-        style: Theme.of(context).textTheme.headline4,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Load On Init Component'),
       ),
-      OutlinedButton(
-          onPressed: () => viewStore.send(LoadOnInitCompnoentAction.increment),
-          child: const Text("increment"))
-    ]);
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(children: [
+              Text(state.name),
+              Text(
+                '${state.count}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              OutlinedButton(
+                  onPressed: () =>
+                      viewStore.send(LoadOnInitCompnoentAction.increment),
+                  child: const Text("increment"))
+            ])
+          ],
+        ),
+      ),
+    );
   }
 }
